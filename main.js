eb45da0b7dbb8b0b03c36d72f5dee2da9891ad42
wajihajafri <jafriwajiha14@gmail.com>
2025-02-06 00:32:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const name ="wajiha" 
+const phoenNumber = 923547889
+
+//console.log(`my name is${name} and my phoneNumber is ${phonenumber}`)
+//console.log(name)
+
+var myName = "wajiha"
+//console.log(myName)
+
+let myName2 ="wajiha"
+//console.log(myName2) // in the following example let and var will be give same output as there is no difference.
+
+//operators
+let number1 = "45"
+let number2 = 45
+//console.log(!number1===number2) //! excalamtion is consider as not(boolean operators) 
+// double == is for comparsion and triple ==== is for comparing datatype
+// 45=45: it will show error as single = sign is considered as assignment operator
+//UNDEFINED AND NULL are datatype
+//undefiend and null are treated as false
+
+// AND = &&
+// OR = ||
+
+const thisisfalse  = false
+const thisistrue = true
+//console.log(thisisfalse===thisistrue)
+
+const myname = "wajiha"
+const mysurname ;
+console.log(mysurname)
+
+let loginid;
+let password = 28976
+ if(!loginid){
+   //console.log("thisiserror")
+ }
